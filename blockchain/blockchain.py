f93b5a6a8e0f6a93c75d8d643f62ccd19d21c535
abhishek30221 <abhishekgusain2705@gmail.com>
2022-02-02 12:40:28 +0530

    created a block

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,30 +1,31 @@
 from flask import Flask, render_template
-from argparse import ArgumentParser
 from time import time
 
-
 class Blockchain:
+
     def __init__(self):
-        self.transactions = []  # contains all transactions
-        self.chain = []  # contains list of blocks
-        self.create_block(0, '00')  # create genesis block
+        self.transactions = []
+        self.chain = []
+        # Create the genesis block
+        self.create_block(0, '00')
 
     def create_block(self, nonce, previous_hash):
         """
-            Add a block of transactions to the blockchain
+        Add a block of transactions to the blockchain
         """
         block = {'block_number': len(self.chain) + 1,
                  'timestamp': time(),
                  'transactions': self.transactions,
                  'nonce': nonce,
-                 'previous_hash': previous_hash
-                 }
-        # reset the current list of transactions
+                 'previous_hash': previous_hash}
+
+        # Reset the current list of transactions
         self.transactions = []
         self.chain.append(block)
 
 
-# Instantiating the Blockchain
+
+# Instantiate the Blockchain
 blockchain = Blockchain()
 
 # Instantiate the Node
@@ -37,9 +38,11 @@ def index():
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser()  # instance of argument parser
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
     parser.add_argument('-p', '--port', default=5001, type=int, help="port to listen to")
     args = parser.parse_args()
     port = args.port
-    app.run(host='127.0.0.1', port=port,
-            debug=True)  # localhost ( debug = true bcz everytime we got changes, so the server will be restarted)
+
+    app.run(host='127.0.0.1', port=port, debug=True)
